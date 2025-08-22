25e868afbdea0bc32c45c6132a0998dd25c6a1e5
kim seulgi <strawcherry78@gmail.com>
2025-08-22 19:18:29 +0900

    config 변경

--- vite.config.js
+++ vite.config.js
@@ -14,14 +14,6 @@ export default defineConfig({
       '@css': fileURLToPath(new URL('./src/assets/css', import.meta.url)),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/learn-gasp/' : '/',
+  // base: process.env.NODE_ENV === 'production' ? '/learn-gasp/' : '/',
+  base: '/learn-gasp/',
 })
-
-// base: '/learn-gasp/',
-// build: {
-//   rollupOptions: {
-//     output: {
-//       manualChunks: undefined,
-//     },
-//   },
-// },
